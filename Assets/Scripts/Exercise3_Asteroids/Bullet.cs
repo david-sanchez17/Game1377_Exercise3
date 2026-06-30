1778979dc520296eb6c41dbadd984d37520b5d9d
david-sanchez17 <david.sanchez17@g.austincc.edu>
2026-06-30 14:13:25 -0500

    Update Bullet.cs

--- Assets/Scripts/Exercise3_Asteroids/Bullet.cs
+++ Assets/Scripts/Exercise3_Asteroids/Bullet.cs
@@ -21,9 +21,12 @@ public class Bullet : MonoBehaviour
     [SerializeField] private float bulletSpeed = 20f;
     [SerializeField] private float bulletLifetime = 5f;
 
+    private Rigidbody2D rb;
+
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
+        rb.linearVelocity = transform.up * bulletSpeed;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
